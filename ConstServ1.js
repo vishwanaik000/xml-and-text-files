5d7eb165f2aeb256477c729b131c219530587321
Vishwa Naik <44199650+vishwanaik000@users.noreply.github.com>
2019-03-08 16:26:30 +0530

    Add files via upload

---
+++ ConstServ1.js
@@ -0,0 +1,17 @@
+	/*//SIT_Dev URL
+      version : '3.0',
+     appName : 'SIT 3',
+     MainUrl : "http://rcsmobile.itcinfotech.com:3001/test/getURL",
+	SIT_DEV_ENDS*/
+     
+    /*// SIT Test URL
+     version : '6.0',
+     appName : 'SIT 1',
+     MainUrl : "https://rcsdqssit.rcsoncloud.com:8443/getURL",
+*/
+	 
+	 /*// UAT URL
+	 version : '4.0',
+     appName : 'UAT 2',
+     MainUrl : "https://rcsdqscb.rcsoncloud.com:8443/getUrl",
+*/
